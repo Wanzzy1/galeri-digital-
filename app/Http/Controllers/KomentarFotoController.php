4936b15e735cf1445c6c4ac7f023290ba67e26e5
Wanzzy1 <arcwahdan@gmail.com>
2024-02-06 09:14:35 +0700

    membuat table foto, komentar foto, album, likefoto

---
+++ app/Http/Controllers/KomentarFotoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KomentarFoto;
+use App\Http\Requests\StoreKomentarFotoRequest;
+use App\Http\Requests\UpdateKomentarFotoRequest;
+
+class KomentarFotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKomentarFotoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KomentarFoto $komentarFoto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KomentarFoto $komentarFoto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKomentarFotoRequest $request, KomentarFoto $komentarFoto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KomentarFoto $komentarFoto)
+    {
+        //
+    }
+}
